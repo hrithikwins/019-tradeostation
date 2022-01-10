8eb5c32a56d233c8bbe24b90ad21264823378676
Hrithik Tiwari <hrithikrtiwari@gmail.com>
2022-01-11 02:08:58 +0530

    added the banner

---
+++ pages/index.jsx
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Index.module.css";
+
+export default function Home() {
+    return (
+        <div>
+            {/* this is the header component */}
+            <div className="container">
+                <div className="d-flex justify-content-between w-100">
+                    <div className="pl-4">Logo</div>
+                    <div className="p-5">Other texts</div>
+                </div>
+            </div>
+            <hr />
+            {/* this is the banner now */}
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div className="d-flex flex-column justify-content-center">
+                            <div className="py-4">
+                                <h1 className={styles.title + " fw-bold"}>
+                                    Your Upgraded version by committing some
+                                    time for yourself{" "}
+                                </h1>
+                            </div>
+                            <p className={styles.paragraph}>
+                                Our Aim at time commit is to help you develop
+                                good habits and achieve your goals.
+                            </p>
+                            <div className="d-flex py-md-5">
+                                <div className="btn btn-success">
+                                    Get Started
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <div className="" style={{ position: "relative" }}>
+                            <Image
+                                src="/images/banner.png"
+                                alt="banner"
+                                width={900}
+                                height={600}
+                                priority={true}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* end banner  */}
+            {/* start container for video */}
+            <div className="container">
+                <div className={styles.cardHeading + " card p-5 text-center"}>
+                    Learn to Leverage the Price Action Strategy and become a Pro
+                    Trader
+                </div>
+            </div>
+        </div>
+    );
+}
